ee7417689dea298be4fd1b6a8d61d9630639673a
Victor Castillo <vicasfer3@gmail.com>
2023-10-12 19:55:59 +0200

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	base: '/05-blackjack-vite-refactor/', //Establece una ruta base a partir de la cual buscará los archivos (importante para deplegar en GitHub Pages)
+	plugins: [] //Se pueden añadir plugins adicionales (consultar documentación)
+});
